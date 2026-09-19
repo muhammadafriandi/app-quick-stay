7a272ed3ccd00354d07056727563d41600a89619
kapalokampuang63 <kapalokampuang63@gmail.com>
2026-09-19 17:12:34 +0700

    update model user

--- server/models/User.js
+++ server/models/User.js
@@ -17,32 +17,14 @@
 
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    _id: {
-      type: String,
-      required: true,
-    },
-
-    email: {
-      type: String,
-      required: true,
-    },
-
-    username: {
-      type: String,
-      required: true,
-    },
-
-    image: {
-      type: String,
-      default: "",
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true, },
+  email: { type: String, required: true, },
+  username: { type: String, required: true, },
+  image: { type: String, default: "", },
+  role: { type: String, enum: ["user", "owner"], default: "user" },
+  recentSearchedCities: [{ type: String }],
+}, { timestamps: true })
 
 const User =
   mongoose.models.User || mongoose.model("User", userSchema);
